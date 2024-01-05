b5d83053c771f39a53216fe1d97eca4b4ffc5989
askar <maeaxvx@gmail.com>
2024-01-05 20:35:35 +0200

    code cleaning and organizing

--- fruits_project/_project/user/urls.py
+++ fruits_project/_project/user/urls.py
@@ -6,29 +6,27 @@ urlpatterns = [
     path('logout',views.logout_user, name='logout' ),
     path('register',views.register, name='register' ),
     path('home',views.home, name='home' ),
-
+# =====================================================================================
     path('addcontainer',views.add_container, name='addcontainer' ),
     path('containerdelete/<int:id>',views.container_delete, name='containerdelete' ),
     path('containerupdate/<int:id>',views.container_update, name='containerupdate' ),
-
-    path('cardetails',views.car_details, name='cardetails' ),
-    path('today',views.today, name='today' ),
-    path('finished',views.finished, name='finished' ),
-    path('remain',views.remain, name='remain' ),
-    path('sellcar',views.sell_car, name='sellcar' ),
-
+    path('condetails',views.container_details, name='cardetails' ),
+    path('today',views.today_containers, name='today' ),
+    path('remain',views.remain_containers, name='remain' ),
+    path('finished',views.finished_containers, name='finished' ),
+    path('sellcon',views.sell_container, name='sellcar' ),
+# =====================================================================================
     path('items',views.add_items, name='items' ),
     path('itemupdate/<int:id>',views.item_update, name='itemupdate' ),
-
+# =====================================================================================
     path('sellerpage/<int:id>',views.seller_page, name='sellerpage' ),
     path('selleraccounts',views.seller_accounts, name='selleraccounts' ),
     path('sellerdelete/<int:id>',views.seller_delete, name='sellerdelete' ),
     path('sellerupdate/<int:id>',views.seller_update, name='sellerupdate' ),
-
+# =====================================================================================
     path('supplierpage/<int:id>',views.supplier_page, name='supplierpage' ),
     path('suppliersaccounts',views.suppliers_accounts, name='suppliersaccounts' ),
     path('suppliersdelete/<int:id>',views.supplier_delete, name='suppliersdelete' ),
     path('supplierupdate/<int:id>',views.supplier_update, name='supplierupdate' ),
-
 ]
     
