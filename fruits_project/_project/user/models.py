316ada85f5b31a3cd35e390c3eb4f827b5668fb5
askar <maeaxvx@gmail.com>
2024-01-05 15:50:23 +0200

    alter supplier table (money), and fetch data

--- fruits_project/_project/user/models.py
+++ fruits_project/_project/user/models.py
@@ -1,27 +1,32 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Supplier(models.Model):
     name = models.CharField(max_length=30)
     place = models.CharField(max_length=70, default='غير محدد')
     date = models.DateField(default=timezone.now().date())
-    
-    def __str__(self) -> str:
+    his_money = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    on_him_money = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+
+    @property
+    def num_containers(self):
+        return self.container_set.count()
+
+    def __str__(self):
         return self.name
-    
+
 class Seller(models.Model):
     name = models.CharField(max_length=30)
     place = models.CharField(max_length=70, default='غير محدد')
     date = models.DateField(default=timezone.now().date())
-    
-    def __str__(self) -> str:
+
+    def __str__(self):
         return self.name
-    
+
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits =5, decimal_places =2)
-    
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
     def __str__(self):
         return self.name
 
@@ -29,10 +34,11 @@ class Container(models.Model):
     supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True)
     items = models.ManyToManyField(Item, null=True)
     date = models.DateField()
-    type = models.CharField(max_length = 30, default='عمولة')
+    type = models.CharField(max_length=30, default='عمولة')
     num_items = models.PositiveIntegerField(null=True)
     num_sold_items = models.PositiveIntegerField(null=True)
     num_not_sold_items = models.PositiveIntegerField(null=True)
 
     def __str__(self):
         return f"Container {self.id} - {self.date}"
+
